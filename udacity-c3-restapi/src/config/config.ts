352a858a65bee065f5d50b1c85d30b3265c961c6
Chris <chris@malouf.de>
2019-10-10 12:23:27 +0200

    fix(typo): Fix spelling for the AWS-region config property

--- udacity-c3-restapi/src/config/config.ts
+++ udacity-c3-restapi/src/config/config.ts
@@ -5,7 +5,7 @@ export const config = {
     "database": process.env.UDCD_POSTGRES_DATABASE,
     "host": process.env.UDCD_POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_reigion": process.env.UDCD_AWS_REGION,
+    "aws_region": process.env.UDCD_AWS_REGION,
     "aws_profile": process.env.UDCD_AWS_PROFILE,
     "aws_media_bucket": process.env.UDCD_AWS_MEDIA_BUCKET
   },
